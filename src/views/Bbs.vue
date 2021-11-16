f74156371282d23ac2ec87637d6352aa84614a0d
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2021-11-16 15:51:05 +0900

    記事一覧表示機能の追加

--- src/views/Bbs.vue
+++ src/views/Bbs.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <div class="ex"></div>
+    <div>投稿者名：<input type="text" v-model="articleName" /></div>
+    <div>
+      投稿内容：<textarea
+        cols="30"
+        rows="10"
+        v-model="articleContent"
+      ></textarea>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Article } from "../types/article";
+@Component
+export default class BbsDetail extends Vue {
+  // 現在の記事一覧
+  private currentArticleList = new Array<Article>();
+}
 </script>
-
 <style></style>
